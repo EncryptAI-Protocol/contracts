82f31050c32186ad201dd8f8c978e8317980150e
joan <joan.flotats@midokura.com>
2024-05-26 20:13:45 +0200

    feat(data source): add contracts

---
+++ src/DataSource.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+struct Information {
+    string name;
+    string symbol;
+    string hash;
+    uint256 price;
+    uint256 fee;
+    string[] labels;
+}
+
+contract DataSource is ERC721, ERC721URIStorage, AccessControl {
+    bytes32 public constant DATA_PROVIDER = keccak256("DATA_PROVIDER");
+
+    string private uri;
+
+    string[] public labels;
+    string public hash;
+    uint256 public price;
+    uint256 public fee;
+
+    constructor(
+        address defaultAdmin,
+        address minter,
+        string memory _name,
+        string memory _symbol,
+        string memory _hash,
+        string memory _uri,
+        uint256 _price,
+        uint256 _fee,
+        string[] memory _labels
+    ) ERC721(_name, _symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(DATA_PROVIDER, minter);
+
+        labels = _labels;
+        hash = _hash;
+        uri = _uri;
+        price = _price;
+        fee = _fee;
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyRole(DATA_PROVIDER) {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function getInfo() public view returns (Information memory) {
+        return Information(super.name(), super.symbol(), hash, price, fee, labels);
+    }
+}
---
+++ src/DataSourceFactory.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./DataSource.sol";
+
+contract DataSourceFactory {
+    DataSource[] public dataSources;
+
+    event DataSourceCreated(address ownerAddress, address dataAddress, string name, uint256 price, uint256 fee);
+
+    function createDataSource(
+        string memory _name,
+        string memory _symbol,
+        string memory _uri,
+        string memory _hash,
+        string[] memory _labels,
+        uint256 _price,
+        uint256 _fee
+    ) public {
+        DataSource dataSource =
+            new DataSource(msg.sender, msg.sender, _name, _symbol, _hash, _uri, _price, _fee, _labels);
+
+        dataSources.push(dataSource);
+
+        emit DataSourceCreated(msg.sender, address(dataSource), _name, _price, _fee);
+    }
+
+    function getDataSources(uint256 limit) external view returns (Information[] memory) {
+        if (limit > dataSources.length) {
+            limit = dataSources.length;
+        }
+
+        Information[] memory info = new Information[](limit);
+
+        for (uint256 i = 0; i < limit; i++) {
+            info[i] = DataSource(dataSources[i]).getInfo();
+        }
+
+        return info;
+    }
+}
